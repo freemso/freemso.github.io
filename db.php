9f1264604c809998392f973ee77cdad1acb2cfab
mso <freemso@hotmail.com>
2016-09-15 13:51:19 +0800

    version 1

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $db_host="localhost";                                           //连接的服务器地址
-    $db_user="root";                                                  //连接数据库的用户名
-    $db_psw="748748";                                                  //连接数据库的密码
-    $db_name="students";                                           //连接的数据库名称
-    $conn = new mysqli($db_host, $db_user, $db_psw, $db_name);     //实例化mysqli
-?>
